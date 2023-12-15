0dc4ff1bd1fa2211ca80d3ef77fa6a65e4785961
LuizFernandoDeveloper <luiz.fernando.developer@outlook.com>
2023-12-15 15:33:29 -0300

    Configuring Eureka clients

--- HR-payroll/src/main/java/com/hrpayroll/entities/Worker.java
+++ HR-payroll/src/main/java/com/hrpayroll/entities/Worker.java
@@ -3,13 +3,6 @@ package com.hrpayroll.entities;
 import java.io.Serializable;
 import java.util.Objects;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
-
-
 public class Worker implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
