dec0e3eb04187533a5c715fa93f767ca9bb54def
LuizFernandoDeveloper <luiz.fernando.developer@outlook.com>
2023-12-15 13:35:40 -0300

    Ribbon load balancing

--- HR-payroll/src/main/java/com/hrpayroll/services/PaymentService.java
+++ HR-payroll/src/main/java/com/hrpayroll/services/PaymentService.java
@@ -1,13 +1,8 @@
 package com.hrpayroll.services;
 
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 import com.hrpayroll.entities.Payment;
 import com.hrpayroll.entities.Worker;
