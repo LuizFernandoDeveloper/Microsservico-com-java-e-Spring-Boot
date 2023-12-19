253a14ac35b696509dd78a6b1815670be95546d3
LuizFernandoDeveloper <luiz.fernando.developer@outlook.com>
2023-12-19 11:43:18 -0300

    Project hr-api-gate

---
+++ HR-api-gateway-zuul/src/main/java/com/zuulserver/HrApiGatewayZuulApplication.java
@@ -0,0 +1,12 @@
+package com.zuulserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrApiGatewayZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrApiGatewayZuulApplication.class, args);
+	}
+}
